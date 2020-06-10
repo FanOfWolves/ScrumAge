1cd79442cd446adaeca261b20530681a8ecf155d
Michael Edwards <EDWARDSMR@ETSU.EDU>
2020-06-10 14:42:32 -0400

    Small change for a test.

--- ScrumageWPF/ScrumageWPF/obj/Debug/netcoreapp3.1/MainWindow.g.cs
+++ ScrumageWPF/ScrumageWPF/obj/Debug/netcoreapp3.1/MainWindow.g.cs
@@ -69,7 +69,7 @@ namespace ScrumageWPF {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/ScrumageWPF_1jcpysrk_wpftmp;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ScrumageWPF;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
