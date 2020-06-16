92a3fd337f0268a1d3b58bb7ab4a670403e9775e
Michael Edwards <EDWARDSMR@ETSU.EDU>
2020-06-16 19:45:34 -0400

    Fixed the entire branch

--- ScrumageWPF/ScrumageWPF/BoardSpace/ResourceNode.cs
+++ ScrumageWPF/ScrumageWPF/BoardSpace/ResourceNode.cs
@@ -5,18 +5,52 @@ using ScrumageEngine.Objects.Player;
 using ScrumageEngine.Objects.Items;
 
 namespace ScrumageEngine.BoardSpace {
-	public class ResourceNode : Node { //D:
+    public class ResourceNode : Node
+    {
+        //D:
 
-		private Resource nodeResource;
-		
-		
-		public ResourceNode(Int32 nodeID, String nodeName):base(nodeID, nodeName) {
+        private Resource nodeResource = null;
+        private const Int32 RESOURCE_BASE_CHANCE = 20;
+        private readonly Random resourceChanceCalculator;
 
-		}
+        public ResourceNode(Int32 nodeID, String nodeName, Resource nodeResource) : base(nodeID, nodeName) {
+            this.nodeResource = nodeResource;
+            this.resourceChanceCalculator = new Random();   
+        }
 
-		public override String DoAction(Player player) {
-			//throw new NotImplementedException();
-			return ""; // This gets added to log
+        private List<Pawn> GatherPlayerPawns(Int32 playerId) {
+            List<Pawn> _playerPawns = Pawns.FindAll(_playerPawn => _playerPawn.PawnID == playerId);
+            Pawns.RemoveAll(_playerPawn => _playerPawn.PawnID == playerId);
+            return _playerPawns;
+        }
+
+        private Boolean RollForResource(Int32 successChance) {
+            Int32 _result = this.resourceChanceCalculator.Next(0,101);
+            return successChance <= _result;
+        }
+
+        
+
+        public override String DoAction(Player player) {
+            List<Pawn> _playerPawns = GatherPlayerPawns(player.PlayerID);
+            
+            Int32 _resourceAcquireChance = RESOURCE_BASE_CHANCE;
+            foreach (Pawn pawn in _playerPawns) {
+                _resourceAcquireChance += nodeResource.GetChance(pawn);
+                player.GivePawn(pawn);
+            }
+
+            Boolean _getResource = RollForResource(_resourceAcquireChance);
+            if (_getResource == true) {
+                //returns the did they do the do thing
+                //player.acquireResource();
+               
+            }
+            else {
+                
+            }
+
+            return ""; // This gets added to log
 		}
 	}
 }
---
+++ ScrumageWPF/ScrumageWPF/Objects/Player/ResourceContainer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using ScrumageEngine.Objects.Items;
+using System.Collections.Generic;
+
+namespace ScrumageEngine.Objects.Player {
+    public class ResourceContainer {
+
+        #region Fields
+        private Dictionary<Resource, Int32> resourceDictionary;
+
+
+        #endregion
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ResourceContainer"/> class.
+        /// </summary>
+        public ResourceContainer() {
+            resourceDictionary = new Dictionary<Resource, Int32>();
+        }
+
+        private void AddResource(Resource re) {
+            //resourceDictionary.Keys[re].value;
+		}
+
+        /// <summary>
+        /// Wrongs the function.
+        /// </summary>
+        private static void WrongFunction() {
+
+		}
+
+
+
+    }
+}
