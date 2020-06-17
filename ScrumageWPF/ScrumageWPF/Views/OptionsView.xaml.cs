7ef5236f6a15d3367eafca1cb06b4d1bc2430db9
Jordan C <nykeztv@users.noreply.github.com>
2020-06-16 21:47:42 -0400

    Initial ViewModel MainMenu design

---
+++ ScrumageWPF/ScrumageWPF/Views/OptionsView.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ScrumageEngine.Views
+{
+    /// <summary>
+    /// Interaction logic for OptionsView.xaml
+    /// </summary>
+    public partial class OptionsView : Page
+    {
+
+        private List<string> _nameList;
+
+        private int MAX_PLAYERS = 4;
+
+        /// <summary>
+        /// Creates a new instance of OptionsViews
+        /// </summary>
+        public OptionsView()
+        {
+            InitializeComponent();
+            Loaded += OnLoad;
+        }
+
+        /// <summary>
+        /// Creates a new options view, with namesList
+        /// </summary>
+        /// <param name="namesList"></param>
+
+        public OptionsView(ref List<string> namesList)
+        {
+            InitializeComponent();
+
+            _nameList = namesList;
+            Loaded += OnLoad;
+
+        }
+
+        /// <summary>
+        /// Called from UI to indicate our panel is loaded
+        /// </summary>
+        private void OnLoad(object sender, RoutedEventArgs e)
+        {
+            // Build player options
+            for (int i = 1; i <= MAX_PLAYERS; i++)
+            {
+                cbPlayerList.Items.Add(i);
+            }
+        }
+
+    }
+}
---
+++ ScrumageWPF/ScrumageWPF/Windows/MainMenu.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ScrumageEngine.Views;
+
+namespace ScrumageEngine.Windows
+{
+    /// <summary>
+    /// Interaction logic for MainMenu.xaml
+    /// </summary>
+    public partial class MainMenu : Window
+    {
+
+        private List<string> playerList;
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Start the Game
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnStartGame_Click(object sender, RoutedEventArgs e)
+        {
+            StartWindow window = new StartWindow();
+            window.Show();
+            this.Close();
+        }
+
+        /// <summary>
+        /// Exit the Application
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnExitGame_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        /// <summary>
+        /// Call <Global>Help Menu</Global>
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnHelp_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Open the options panel to configure the game
+        /// This can also be done when the Start Game button is pressed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnOptions_Click(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new OptionsView(ref playerList);
+        }
+    }
+}
