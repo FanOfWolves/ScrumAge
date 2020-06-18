562465eec4a22afdeda7299543767c42acbc2bee
Harrison Pollitte <pollitteh@etsu.edu>
2020-06-17 20:09:47 -0400

    Added more to Card.cs

---
+++ ScrumageWPF/ScrumageWPF/Objects/Items/Cards/Card.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Navigation;
+using ScrumageEngine.Objects.Player;
+
+namespace ScrumageEngine.Objects.Items.Cards {
+    /// <summary>
+    /// Abstract class representing cards
+    /// </summary>
+    abstract class Card {
+
+        #region Fields
+
+        private ResourceContainer cardRequirements;
+        private String cardDesc;
+        private String cardName;
+        private String cardFormat;
+        private Int32 cardEndGameBonus;//TODO: Replace with CardBonus class?
+
+        #endregion
+
+        #region Constructors        
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Card"/> class.
+        /// </summary>
+        public Card(ResourceContainer cardRequirementsP) {
+            this.cardRequirements = cardRequirementsP;
+        }
+        #endregion
+
+        #region Methods
+
+        public abstract String Display();
+        public abstract override String ToString();
+        public ResourceContainer GetCardRequirements() {
+            return this.cardRequirements;//TODO: Shallow reference?
+        }
+
+        public Boolean TryPayCost(ResourceContainer payment) {
+            for (Int32 i = 0; i < 4; i++) {
+                Int32 _amountHere = this.cardRequirements.GetResourceAmount()
+            }
+        }
+        #endregion
+    }
+}
