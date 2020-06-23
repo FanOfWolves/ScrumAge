5392255e1e5da36254f75374f0be6ce6e4d7baab
Harrison Pollitte <pollitteh@etsu.edu>
2020-06-23 13:47:28 -0400

    Added testing branch

---
+++ ScrumageWPF/ScrumageWPF.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+
+namespace ScrumageWPF.Test {
+    [TestFixture]
+    public class Tests {
+        [SetUp]
+        public void Setup() {
+        }
+
+        [Test]
+        public void Test1() {
+            Assert.Pass();
+        }
+    }
+}
