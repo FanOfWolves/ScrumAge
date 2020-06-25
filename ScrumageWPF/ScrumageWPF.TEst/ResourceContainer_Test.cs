4c2564fb1dcec054b9b4ba498758c382ee08c008
Harrison Pollitte <pollitteh@etsu.edu>
2020-06-25 17:13:45 -0400

    Added test methods to ResourceContainer_Test

--- ScrumageWPF/ScrumageWPF.TEst/ResourceContainer_Test.cs
+++ ScrumageWPF/ScrumageWPF.TEst/ResourceContainer_Test.cs
@@ -1,8 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Printing;
 using System.Runtime.CompilerServices;
 using System.Text;
 using NUnit.Framework;
+using ScrumageEngine.Objects.Items;
+using ScrumageEngine.Objects.Player;
+using ScrumageEngine.BoardSpace;
+using ScrumageEngine;
+
 namespace ScrumageWPF.Test {
 
     /// <summary>
@@ -11,7 +17,73 @@ namespace ScrumageWPF.Test {
     [TestFixture]
     class ResourceContainer_Test {
 
+        
+        #region Category: Instantiation
+        [Test]
+        [Category("Instantiation")]
+        public void ResourceContainer_Instantiates() {
 
+        }
+        #endregion
 
+        #region Category: Operators
+        [Test]
+        [Category("Operators")]
+        public void ResourceContainer_ComparisonOperatorsCorrectlyStateIfContainerHasEnoughResources() {
+
+        }
+
+        [Test]
+        [Category("Operators")]
+        public void ResourceContainer_MinusOperatorRemovesFromThisResourceContainer() {
+
+        }
+
+        [Test]
+        [Category("Operators")]
+        public void ResourceContainer_AdditionOperatorAddsToThisResourceContainer() {
+
+        }
+        #endregion
+
+        #region Category: Dictionary Handling
+        [Test]
+        [Category("Dictionary Handling")]
+        public void ResourceContainer_AddsToExisting() {
+
+        }
+
+        [Test]
+        [Category("Dictionary Handling")]
+        public void ResourceContainer_DoesNotRemoveResourceClass() {
+
+        }
+
+        [Test]
+        [Category("Dictionary Handling")]
+        public void ResourceContainer_IntegerIndexerRetrievesResourceAmount() {
+
+        }
+        #endregion
+
+        #region Category: IEnumerable<T> Interface
+        [Test]
+        [Category("IEnumerable<T> Interface")]
+        public void ResourceContainer_IsIEnumerable() {
+
+        }
+        #endregion
+
+        #region Category: Referencing and Equality
+        [Test]
+        [Category("Referencing and Equality")]
+        public void ResourceContainer_ReturnsDeepCopyNotOriginalResource() {
+
+        }
+        #endregion
+
+        #region Category: Other
+
+        #endregion
     }
 }
