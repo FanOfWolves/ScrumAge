0110a304ef9c55d7a55f67de2ed39ec89923d39d
Harrison Pollitte <pollitteh@etsu.edu>
2020-06-25 16:08:03 -0400

    Added ResourceContainer_Test.cs Testing suite

---
+++ ScrumageWPF/ScrumageWPF.TEst/ResourceContainer_Test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using NUnit.Framework;
+namespace ScrumageWPF.Test {
+
+    /// <summary>
+    /// Tests for the ResourceContainer.cs class
+    /// </summary>
+    [TestFixture]
+    class ResourceContainer_Test {
+
+
+
+    }
+}
