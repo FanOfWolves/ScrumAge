bca510c223107ec2c067dc56c65764f55db51a8b
Harrison Pollitte <pollitteh@etsu.edu>
2020-06-26 16:53:27 -0400

    Readded operator test

--- ScrumageWPF/ScrumageWPF.TEst/ResourceContainer_Test.cs
+++ ScrumageWPF/ScrumageWPF.TEst/ResourceContainer_Test.cs
@@ -16,7 +16,7 @@ namespace ScrumageWPF.Test {
     /// </summary>
     [TestFixture]
     class ResourceContainer_Test {
-    
+
         #region Category: Instantiation
         [Test]
         [Category("Instantiation")]
@@ -27,13 +27,73 @@ namespace ScrumageWPF.Test {
             Assert.That(con1[new Implementation()] == 0);
             Assert.That(con1[new Testing()] == 0);
         }
+
+        [Test]
+        [Category("Instantiation")]
+        [TestCase(0, 0, 0, 0)]
+        [TestCase(1, 0, 0, 0)]
+        [TestCase(9, 4, 5, 6)]
+        public void ResourceContainer_ParameterInstantiatesCorrectly(Int32 req, Int32 des, Int32 imp, Int32 tes) {
+            ResourceContainer container = new ResourceContainer(new Int32[] { req, des, imp, tes });
+            Assert.That(container[new Requirements()] == req);
+            Assert.That(container[new Design()] == des);
+            Assert.That(container[new Implementation()] == imp);
+            Assert.That(container[new Testing()] == tes);
+        }
+
         #endregion
 
         #region Category: Operators
         [Test]
         [Category("Operators")]
-        public void ResourceContainer_ComparisonOperatorsCorrectlyStateIfContainerHasEnoughResources() {
+        [TestCase(new Int32[] { 0, 0, 0, 0 }, new Int32[] { 0, 0, 0, 0 })]
+        [TestCase(new Int32[] { 1, 1, 1, 1 }, new Int32[] { 0, 0, 0, 1 })]
+        [TestCase(new Int32[] { 2, 3, 2, 2 }, new Int32[] { 2, 2, 2, 2 })]
 
+        [TestCase(new Int32[] { 3, 5, 3, 3 }, new Int32[] { 2, 4, 2, 2 })]
+
+        public void ResourceContainer_ComparisonOperatorsStateIfContainerHasEnoughResources(Int32[] inventory, Int32[] cost) {
+            ResourceContainer payment = new ResourceContainer(inventory);
+            ResourceContainer toPay = new ResourceContainer(cost);
+
+            Assert.That(payment >= toPay);
+        }
+
+        [Test]
+        [TestCase(new Int32[] { 0, 0, 0, 0 }, new Int32[] { 1, 1, 0, 0 })]
+        [TestCase(new Int32[] { 0, 0, 0, 0 }, new Int32[] { 1, 1, 1, 1 })]
+        [TestCase(new Int32[] { 1, 1, 1, 1 }, new Int32[] { 1, 1, 1, 2 })]
+        [TestCase(new Int32[] { 2, 3, 2, 2 }, new Int32[] { 2, 4, 2, 2 })]
+        public void ResourceContainer_ComparisonOperatorsStateIfContainerDoesNotHaveEnoughResources(Int32[] inventory, Int32[] cost) {
+            ResourceContainer payment = new ResourceContainer(inventory);
+            ResourceContainer toPay = new ResourceContainer(cost);
+
+            Assert.That(payment <= toPay);
+        }
+
+
+        [Test]
+        [Category("Operators")]
+        [TestCase(new Int32[] { 0, 0, 0, 0 }, new Int32[] { 0, 0, 0, 0 })]
+        [TestCase(new Int32[] { 1, 1, 1, 1 }, new Int32[] { 0, 0, 0, 1 })]
+        [TestCase(new Int32[] { 2, 3, 2, 2 }, new Int32[] { 2, 2, 2, 2 })]
+        [TestCase(new Int32[] { 2, 3, 2, 2 }, new Int32[] { 2, 4, 2, 2 })]
+        [TestCase(new Int32[] { 3, 5, 3, 3 }, new Int32[] { 2, 4, 2, 2 })]
+        [TestCase(new Int32[] { 1, 1, 1, 1 }, new Int32[] { 1, 1, 1, 2 })]
+        public void ResourceContainer_ComparisonOperatorsDoNotAlterState(Int32[] inventory, Int32[] cost) {
+            ResourceContainer payment = new ResourceContainer(inventory);
+            ResourceContainer toPay = new ResourceContainer(cost);
+            if(payment >= toPay)
+                ;
+
+            Assert.That(payment[new Requirements()] == inventory[0]);
+            Assert.That(payment[new Design()] == inventory[1]);
+            Assert.That(payment[new Implementation()] == inventory[2]);
+            Assert.That(payment[new Testing()] == inventory[3]);
+            Assert.That(toPay[new Requirements()] == cost[0]);
+            Assert.That(toPay[new Design()] == cost[1]);
+            Assert.That(toPay[new Implementation()] == cost[2]);
+            Assert.That(toPay[new Testing()] == cost[3]);
         }
 
         [Test]
