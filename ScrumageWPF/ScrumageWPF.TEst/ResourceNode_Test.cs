ed42b4b6c69449b0fc3465f39a2afc140b3abb01
Harrison Pollitte <pollitteh@etsu.edu>
2020-06-30 18:47:45 -0400

    Added ResourceNode_Test.cs

---
+++ ScrumageWPF/ScrumageWPF.TEst/ResourceNode_Test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ScrumageEngine.BoardSpace;
+using ScrumageEngine.Objects.Items;
+using ScrumageEngine.Objects.Player;
+namespace ScrumageWPF.Test {
+
+    /// <summary>
+    /// Testing class for <see cref="ResourceNode"/>
+    /// </summary>
+    class ResourceNode_Test {
+    }
+}
