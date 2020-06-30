4c3c12c884df282449df4449da42a9075cf65aed
Harrison Pollitte <pollitteh@etsu.edu>
2020-06-30 18:49:51 -0400

    Fixed ResourceNode_Test formatting

--- ScrumageWPF/ScrumageWPF.TEst/ResourceNode_Test.cs
+++ ScrumageWPF/ScrumageWPF.TEst/ResourceNode_Test.cs
@@ -4,16 +4,18 @@ using System.Text;
 using ScrumageEngine.BoardSpace;
 using ScrumageEngine.Objects.Items;
 using ScrumageEngine.Objects.Player;
+using NUnit.Framework;
 namespace ScrumageWPF.Test {
 
     /// <summary>
     /// Testing class for <see cref="ResourceNode"/>
     /// </summary>
+    [TestFixture]
     class ResourceNode_Test {
 
 
         #region Category: Instantiation
-
+        [Test]
 
         #endregion
 
