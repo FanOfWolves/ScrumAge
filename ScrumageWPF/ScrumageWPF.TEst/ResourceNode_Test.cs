6c569574ac77d07a1f8a5797c3d1bea0329ba1a2
Harrison Pollitte <pollitteh@etsu.edu>
2020-06-30 19:17:32 -0400

    Added more tests

--- ScrumageWPF/ScrumageWPF.TEst/ResourceNode_Test.cs
+++ ScrumageWPF/ScrumageWPF.TEst/ResourceNode_Test.cs
@@ -13,23 +13,41 @@ namespace ScrumageWPF.Test {
     [TestFixture]
     class ResourceNode_Test {
 
+        private const Int32 REQ = 0;
+        private const Int32 DES = 1;
+        private const Int32 IMP = 2;
+        private const Int32 TES = 3;
+
+
+        private Resource CreateResource(Int32 type) {
+            if(type == REQ)
+                return new Requirements();
+            else if(type == DES)
+                return new Design();
+            else if(type == IMP)
+                return new Implementation();
+            return new Testing();
+        }
+
 
         #region Category: Instantiation
         
         
         [Test]
         #region Test-Cases
-        [TestCase(0, "Node1")]
-        [TestCase(6, "Albert")]
-        [TestCase(888, "Node943")]
-        [TestCase()]
-        [TestCase(0, null)]
+        [TestCase(0, "Node1", typeof(Implementation))]
+        [TestCase(6, "Albert", typeof(Implementation))]
+        [TestCase(888, "Node943", typeof(Implementation))]
+        [TestCase(1, "null", ]
         #endregion
-        public void ResourceNode_ConstructorInstantiatesCorrectly(Int32 id, String name) {
-
+        public void ResourceNode_ConstructorInstantiatesCorrectly(Int32 id, String name, Resource theResource) {
+            Node testNode = new ResourceNode(id, name, theResource);
+            Assert.That(theResource, Is.EqualTo(new Implementation()));
         }
         
             
+
+
         #endregion
 
 
