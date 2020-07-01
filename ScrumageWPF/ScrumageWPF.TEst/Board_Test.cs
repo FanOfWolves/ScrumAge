fbfc378bd02a559083c7fa6247f5de880e70ad06
Harrison Pollitte <pollitteh@etsu.edu>
2020-07-01 16:51:34 -0400

    Added Board_Test.cs

---
+++ ScrumageWPF/ScrumageWPF.TEst/Board_Test.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using ScrumageEngine.BoardSpace;
+using ScrumageEngine.Objects.Items.Cards;
+using ScrumageEngine.Objects.Items;
+using ScrumageEngine.Objects.Player;
+
+namespace ScrumageWPF.Test {
+
+    /// <summary>
+    /// Testing class for <see cref="ScrumageEngine.BoardSpace.Board"/>.
+    /// </summary>
+    [TestFixture]
+    class Board_Test {
+        #region Fields
+        Board testBoard;
+
+        #endregion
+
+        #region Test Class Helper Methods
+
+        #endregion
+
+        #region Board Tests
+
+        #region Category: Instantiation
+
+
+
+        #endregion
+
+
+
+        #endregion
+
+    }
+}
