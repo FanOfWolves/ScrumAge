b6e7e6a5bf2451e19b52cbd5bf1a4bed140d4348
Harrison Pollitte <pollitteh@etsu.edu>
2020-07-01 14:51:44 -0400

    Added UpgradeNode_Test.cs

---
+++ ScrumageWPF/ScrumageWPF.TEst/UpgradeNode_Test.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using ScrumageEngine.BoardSpace;
+using ScrumageEngine.Objects.Player;
+using ScrumageEngine.Objects.Items;
+
+namespace ScrumageWPF.Test {
+
+    /// <summary>
+    /// Testing class for <see cref="ScrumageEngine.BoardSpace.UpgradeNode"/>.
+    /// See also <seealso cref="ScrumageEngine.BoardSpace.Node"/>.
+    /// </summary>
+    [TestFixture]
+    class UpgradeNode_Test {
+
+        #region Fields
+
+        #endregion
+
+        #region Testing Class Helper Methods
+
+        #endregion
+
+        #region UpgradeNode Tests
+
+        #region Category: Instantiation
+
+        #endregion
+
+        #region Category: UpgradeNode.DoAction
+
+        #endregion
+
+        #region Category: General DoAction
+
+        #endregion
+
+        #region Category: Other
+
+        #endregion
+
+        #endregion
+
+    }
+}
