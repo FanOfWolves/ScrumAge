698c6ff63fbc7c95898af5f19d48cc48b549cb1d
Harrison Pollitte <pollitteh@etsu.edu>
2020-07-01 15:41:48 -0400

    Added returning pawn testing

--- ScrumageWPF/ScrumageWPF.TEst/UpgradeNode_Test.cs
+++ ScrumageWPF/ScrumageWPF.TEst/UpgradeNode_Test.cs
@@ -5,6 +5,7 @@ using NUnit.Framework;
 using ScrumageEngine.BoardSpace;
 using ScrumageEngine.Objects.Player;
 using ScrumageEngine.Objects.Items;
+using NUnit.Framework.Internal;
 
 namespace ScrumageWPF.Test {
 
@@ -108,7 +109,7 @@ namespace ScrumageWPF.Test {
             Assert.That(createdNode2, Has.Property("Pawns").Count.EqualTo(0).
                                       And.Property("NodeID").EqualTo(id).
                                       And.Property("NodeName").EqualTo(name));
-        } 
+        }
         #endregion
 
         #endregion
@@ -118,6 +119,59 @@ namespace ScrumageWPF.Test {
         #endregion
 
         #region Category: General DoAction
+
+        #region UpgradeNode_DoAction_DoesNotReturnPawnsToIncorrectPlayer        
+        /// <summary>
+        /// Asserts that <see cref="UpgradeNode.DoAction(Player)"/> does not return node pawns to incorrect players.
+        /// </summary>
+        [Test]
+        [Category("General DoAction")]
+        public void UpgradeNode_DoAction_DoesNotReturnPawnsToIncorrectPlayer() {
+            Int32 originalPlayerPawnCount1 = testPlayer1.Pawns.Count;
+
+            testNode.AddPawn(testPlayer2.TakePawn("Back End"));
+            testNode.AddPawn(testPlayer2.TakePawn("Front End"));
+
+            Int32 originalNodePawnCount = testNode.Pawns.Count;
+
+            testNode.DoAction(testPlayer1);
+            // Assert that node contains only pawns belonging to testPlayer2.
+            Assert.That(testNode.Pawns, Has.All.Property("PawnID").EqualTo(2));
+            
+            // Assert no change in testNode or testPlayer1
+            Assert.That(testNode, Has.Property("Pawns").Count.EqualTo(originalNodePawnCount));
+            Assert.That(testPlayer1.Pawns.Count, Is.EqualTo(originalPlayerPawnCount1));
+        }
+        #endregion
+
+        #region  UpgradeNode_DoAction_ReturnsCorrectPawnsToPlayer
+        /// <summary>
+        /// Asserts that the pawn in the node is correctly returned to the player
+        /// </summary>
+        [Test]
+        [Category("General DoAction")]
+        public void UpgradeNode_DoAction_ReturnsCorrectPawnsToCorrectPlayer() {
+            Int32 originalPlayerPawnCount1 = testPlayer1.Pawns.Count;
+            Int32 originalPlayerPawnCount2 = testPlayer2.Pawns.Count;
+            testNode.AddPawn(testPlayer1.TakePawn("Back End"));
+            testNode.AddPawn(testPlayer2.TakePawn("Back End"));
+            testNode.AddPawn(testPlayer2.TakePawn("Front End"));
+
+            testNode.DoAction(testPlayer1);
+            testNode.DoAction(testPlayer2);
+
+            Int32 newPawnCount1 = testPlayer1.Pawns.Count;
+            Int32 newPawnCount2 = testPlayer2.Pawns.Count;
+
+            // Assert that testPlayer1 has received a pawn back.
+            Assert.That(newPawnCount1, Is.EqualTo(originalPlayerPawnCount1));
+            // Assert that testPlayer2 has received two pawns back.
+            Assert.That(newPawnCount2, Is.EqualTo(originalPlayerPawnCount2));
+
+            // Assert that testNode removes its pawns.
+            Assert.That(testNode.Pawns.Count, Is.EqualTo(0));
+        }
+        #endregion
 
         #endregion
 
