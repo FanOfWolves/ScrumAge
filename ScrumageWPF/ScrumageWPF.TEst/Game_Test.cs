bd35c7f6fa0d8a4f3d4975390b5153ca062c2a78
Michael Edwards <EDWARDSMR@ETSU.EDU>
2020-07-03 17:58:11 -0400

    Setup/Teardown

---
+++ ScrumageWPF/ScrumageWPF.TEst/Game_Test.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using ScrumageEngine.BoardSpace;
+
+namespace ScrumageWPF.Test {
+	[TestFixture]
+	class Game_Test {
+		public List<String> names = new List<String>();
+		public Game game;
+
+		#region Setup and Teardown
+		[OneTimeSetUp]
+		public void Game_Setup() {
+			names.Add("Player 1");
+			names.Add("Player 2");
+			names.Add("Bob");
+			names.Add("Amy");
+			game = new Game(names);
+		}
+
+		[OneTimeTearDown]
+		public void Game_TearDown() {
+			game = null;
+		}
+		#endregion
+
+	}
+}
