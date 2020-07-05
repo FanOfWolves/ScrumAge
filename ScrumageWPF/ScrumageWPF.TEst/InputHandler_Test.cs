8527dd8db2b17043f963527c852aa75789102609
Harrison Pollitte <pollitteh@etsu.edu>
2020-07-04 20:12:22 -0400

    Created InputHandler_Test

---
+++ ScrumageWPF/ScrumageWPF.TEst/InputHandler_Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ScrumageEngine.InputLogic;
+using ScrumageEngine.BoardSpace;
+using ScrumageEngine.Objects.Player;
+using ScrumageEngine.Objects.Items;
+using NUnit.Framework;
+
+namespace ScrumageWPF.Test {
+    /// <summary>
+    /// Testing class for <see cref="InputHandler"/>.
+    /// </summary>
+    [TestFixture]
+    class InputHandler_Test {
+
+
+
+
+
+
+    }
+}
