6728dd4678a05c8dcb1d0e363e5b9d54430cef49
Harrison Pollitte <pollitteh@etsu.edu>
2020-07-05 19:23:07 -0400

    Added Comment

--- ScrumageWPF/ScrumageWPF/Exceptions/MovePawnException.cs
+++ ScrumageWPF/ScrumageWPF/Exceptions/MovePawnException.cs
@@ -1,11 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace ScrumageEngine.Exceptions {
 
+    /// <summary>
+    /// Thrown by InputHandler for incorrect pawn movement.
+    /// </summary>
+    /// <seealso cref="System.Exception" />
     [System.Serializable]
     public class MovePawnException : Exception {
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MovePawnException"/> class.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
         public MovePawnException(String message = "Something went wrong while moving pawn.") : base(message) { }
     }
 }
